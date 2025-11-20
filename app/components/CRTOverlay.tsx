7eb091a81d14fa01559de40f2fa7e4aa077749ea
jiro <jiroaku@proton.me>
2025-11-19 21:05:05 -0600

    feat: add retro CRT overlay effect

---
+++ app/components/CRTOverlay.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export function CRTOverlay() {
+  return (
+    <div className="crt-overlay">
+      <div className="crt-scanlines"></div>
+      <div className="crt-static"></div>
+      <div className="crt-vignette"></div>
+    </div>
+  );
+}
+
